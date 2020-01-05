c5430ae200fd0cca3ba0bee4dcdf8edf72315bcd
jagiello430 <jagiello430@o2.pl>
2020-01-05 17:23:43 +0000

    Stream 8.2

--- src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,25 +1,25 @@
 package com.kodilla.exception.test;
+
 import java.time.LocalTime;
 
 public class FirstChallenge {
+
     public double divide(double a, double b) throws ArithmeticException {
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
-        } catch (Exception e) {
-            System.out.println("Somethink is wrong: " + "\n" + e);
-        } finally {
-            System.out.println("Date: " + LocalTime.now());
-        }return a / b;
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
     }
 
     public static void main(String[] args) {
-
         FirstChallenge firstChallenge = new FirstChallenge();
-
-        double result = firstChallenge.divide(3,0 );
-
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        }catch (ArithmeticException e){
+            System.err.println("Somethink is wrong...   "  + e);
+        }finally {
+            System.out.println(LocalTime.now());
+        }
     }
 }
