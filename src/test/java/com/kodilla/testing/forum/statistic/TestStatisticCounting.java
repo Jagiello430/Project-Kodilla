f95923d946e8a39645f7531db0fbefc3d6cec49e
jagiello430 <jagiello430@o2.pl>
2020-01-15 12:57:42 +0000

    project6.5

--- src/test/java/com/kodilla/testing/forum/statistic/TestStatisticCounting.java
+++ src/test/java/com/kodilla/testing/forum/statistic/TestStatisticCounting.java
@@ -11,15 +11,100 @@ import static org.mockito.Mockito.when;
 
 public class TestStatisticCounting {
     @Test
-    public void testHowListUserIsNull(){
+    public void testAsTheNumberOfUserIsANull(){
         Statistic statistics = mock(Statistic.class);
         StatisticCounting statisticCounting = new StatisticCounting(statistics);
         List<String> testList = new ArrayList<>();
 
         when(statistics.usersNames()).thenReturn(testList);
 
-        int testPostIs0 = statisticCounting.quantityUser();
+        int testUserIs0 = statisticCounting.quantityUser();
+
+        Assert.assertEquals(0,testUserIs0);
+    }
+    @Test
+    public void testAsTheNumberOfUserIsAThousand(){
+        Statistic statistics = mock(Statistic.class);
+        StatisticCounting statisticCounting = new StatisticCounting(statistics);
+        List<String> testList = new ArrayList<>();
+        for (int i = 0; i < 1000; i++){
+            testList.add("aaa");
+        }
+        when(statistics.usersNames()).thenReturn(testList);
+
+        int testUserIs1000 = statisticCounting.quantityUser();
+
+        Assert.assertEquals(1000,testUserIs1000);
+    }
+    @Test
+    public void testAsTheNumberOfPostIsANull(){
+        Statistic statistics = mock(Statistic.class);
+        StatisticCounting statisticCounting = new StatisticCounting(statistics);
+        int postsCount = 0;
+
+        when(statistics.postsCount()).thenReturn(postsCount);
+
+        int testPostIs0 = statisticCounting.quantityPost();
 
         Assert.assertEquals(0,testPostIs0);
+
+    }
+    @Test
+    public void testAsTheNumberOfPostIsAThousand(){
+        Statistic statistics = mock(Statistic.class);
+        StatisticCounting statisticCounting = new StatisticCounting(statistics);
+        int postsCount = 1000;
+
+        when(statistics.postsCount()).thenReturn(postsCount);
+
+        int testPostIs1000 = statisticCounting.quantityPost();
+
+        Assert.assertEquals(1000,testPostIs1000);
+
+    }
+    @Test
+    public void testAsTheNumberOfCommentsIsANull(){
+        Statistic statistics = mock(Statistic.class);
+        StatisticCounting statisticCounting = new StatisticCounting(statistics);
+        int commentsCount = 0;
+
+        when(statistics.commentsCount()).thenReturn(commentsCount);
+
+        int testCommentsIs0 = statisticCounting.quantityCommit();
+
+        Assert.assertEquals(0,testCommentsIs0);
+
+    }
+    @Test
+    public void testAsTheNumberOfCommentsIsSmallerThanPosts(){
+        Statistic statistics = mock(Statistic.class);
+        StatisticCounting statisticCounting = new StatisticCounting(statistics);
+        int commentsCount = 44;
+        int postsCount = 45;
+
+        when(statistics.commentsCount()).thenReturn(commentsCount);
+        when(statistics.postsCount()).thenReturn(postsCount);
+
+        boolean postsIsBigger = statisticCounting.quantityPost() >
+                statisticCounting.quantityCommit();
+
+        Assert.assertTrue(postsIsBigger);
+
+    }
+    @Test
+    public void testAsTheNumberOfCommentsIsBiggerThanPosts(){
+        Statistic statistics = mock(Statistic.class);
+        StatisticCounting statisticCounting = new StatisticCounting(statistics);
+        int commentsCount = 44;
+        int postsCount = 43;
+
+        when(statistics.commentsCount()).thenReturn(commentsCount);
+        when(statistics.postsCount()).thenReturn(postsCount);
+
+        boolean commitIsBigger = statisticCounting.quantityPost() <
+                statisticCounting.quantityCommit();
+
+        Assert.assertTrue(commitIsBigger);
+
     }
 }
