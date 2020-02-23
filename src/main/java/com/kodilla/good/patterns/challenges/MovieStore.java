b318365e1fcc0dd90cb53dabec775edce31d3a44
jagiello430 <jagiello430@o2.pl>
2020-02-23 17:47:07 +0000

    project 13.1

---
+++ src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,49 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MovieStore {
+    public Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+    public List<String> wordString() {
+        List<String> wordString = new ArrayList<>();
+        wordString.add(getMovies().get("IM").get(0));
+        wordString.add(getMovies().get("IM").get(1));
+        wordString.add(getMovies().get("AV").get(0));
+        wordString.add(getMovies().get("AV").get(1));
+        wordString.add(getMovies().get("FL").get(0));
+        wordString.add(getMovies().get("FL").get(1));
+
+        return wordString;
+    }
+    public String words(){
+        return wordString().stream()
+                .collect(Collectors.joining("! "));
+
+    }
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        System.out.println(movieStore.words());
+    }
+}
