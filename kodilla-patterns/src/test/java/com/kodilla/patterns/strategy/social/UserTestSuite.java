aea8dc8adb1b77f002ecf60491ec793dcf85b9a7
jagiello430 <jagiello430@o2.pl>
2020-03-15 20:30:03 +0000

    Strategy

--- kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -1,4 +1,43 @@
 package com.kodilla.patterns.strategy.social;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserTestSuite {
+
+    @Test
+    public void testDefaultSharingStrategies() {
+        //Given
+        User janusz = new Millenials("Janusz Kowalski");
+        User grażyna = new YGeneration("Grażyna Kowalska");
+        User zbigniewo = new ZGeneration("Zbigniew Nowak");
+
+        //When
+        String januszUse = janusz.sharePost();
+        System.out.println("Janusz use: " + januszUse);
+        String grażynaUse = grażyna.sharePost();
+        System.out.println("Grażyna use: " + grażynaUse);
+        String zbigniewoUse = zbigniewo.sharePost();
+        System.out.println("Zbigniewo use: " + zbigniewoUse);
+
+        //Then
+        Assert.assertEquals("SC",januszUse);
+        Assert.assertEquals("TW",grażynaUse);
+        Assert.assertEquals("FB",zbigniewoUse);
+    }
+    @Test
+    public void testIndividualSharingStrategy() {
+        //Given
+        User janusz = new Millenials("Janusz Kowalski");
+
+        //When
+        String januszUse = janusz.sharePost();
+        System.out.println("Janusz use: " + januszUse);
+        janusz.setSocialPublisher(new FacebookPublisher());
+        januszUse = janusz.sharePost();
+        System.out.println("Janusz use: " + januszUse);
+
+        //Then
+        Assert.assertEquals("FB",januszUse);
+    }
 }
