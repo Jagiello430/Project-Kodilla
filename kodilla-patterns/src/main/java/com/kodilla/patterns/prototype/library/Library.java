93efe4ebb60b3574e4a37a5db8eaa0c50bcd7950
jagiello430 <jagiello430@o2.pl>
2020-03-15 11:05:53 +0000

    Prototype

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -1,4 +1,47 @@
 package com.kodilla.patterns.prototype.library;
 
-public class Library {
+import com.kodilla.patterns.prototype.Prototype;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library extends Prototype {
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        return "Library: " +
+                " books=" + books;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library)super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library)super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for (Book theBook : books) {
+            Book clonedList = new Book(theBook.getTitle(),theBook.author,theBook.publicationDate);
+            clonedLibrary.getBooks().add(clonedList);
+        }
+        return clonedLibrary;
+    }
 }
