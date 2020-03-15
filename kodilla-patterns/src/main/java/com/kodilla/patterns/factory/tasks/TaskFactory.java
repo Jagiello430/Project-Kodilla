1de30f960b3ce5d65726f4641970260063161dc0
jagiello430 <jagiello430@o2.pl>
2020-03-15 22:35:55 +0000

    Factory

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/TaskFactory.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns.factory.tasks;
+
+public final class TaskFactory {
+    static Task task;
+
+    public static final String SHOPPINGTASK = "SHOPPINGTASK";
+    public static final String PAINTINGTASK = "PAINTINGTASK";
+    public static final String DRIVINGTASK = "DRIVINGTASK";
+
+    public final Task makeTask(final String taskClass) {
+        switch (taskClass) {
+            case SHOPPINGTASK:
+                return new ShoppingTask("Shopping task","computer", 5000);
+            case PAINTINGTASK:
+                return new PaintingTask("Painting task", "grey", "wall");
+            case DRIVINGTASK:
+                return new DrivingTask("Driving task", "praha","yes");
+            default:
+                return null;
+        }
+    }
+}
